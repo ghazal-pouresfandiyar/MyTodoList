06d900f098e77ffceb343a2adcc40f395fbec488
Ghazal Pouresfandiyar <ghazal.brn@gmail.com>
2022-08-21 11:29:51 +0430

    add go file (todo list REST_API) and dependencies

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module TODO_List
 
 go 1.16
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ src/Main.go
@@ -0,0 +1,123 @@
+package src
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+)
+
+type Status int
+
+const (
+	Undone Status = iota
+	Doing
+	Done
+)
+
+type Todo struct {
+	ID           string `json:"id"`
+	Item         Item   `json:"item"`
+	Status       string `json:"status"`
+	DueDate      string `json:"due_date"`
+	DurationTime int    `json:"duration_time"`
+}
+
+func makeTodoObj(ID string, Item Item, Status Status, DueDate string, DurationTime int) Todo {
+	str := ""
+	switch Status {
+	case Done:
+		str = "Done"
+	case Doing:
+		str = "Doing"
+	case Undone:
+		str = "Undone"
+	default:
+		str = "Undefined"
+	}
+	return Todo{ID, Item, str, DueDate, DurationTime}
+}
+
+type Item struct {
+	Name    string
+	SubItem []string
+}
+
+var todos []Todo
+
+func getTodos(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(todos)
+}
+
+func getTodo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range todos {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Todo{})
+}
+
+func createTodo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var todo Todo
+	_ = json.NewDecoder(r.Body).Decode(&todo)
+	todo.ID = strconv.Itoa(rand.Int()) //create random id and convert it to string (not secure)
+	todos = append(todos, todo)
+	json.NewEncoder(w).Encode(todo)
+}
+
+func updateTodo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for idx, item := range todos {
+		if item.ID == params["id"] {
+			todos = append(todos[:idx], todos[idx+1:]...)
+			var todo Todo
+			_ = json.NewDecoder(r.Body).Decode(&todo)
+			todo.ID = strconv.Itoa(rand.Int()) //create random id and convert it to string (not secure)
+			todos = append(todos, todo)
+			json.NewEncoder(w).Encode(todo)
+			return
+		}
+	}
+}
+
+func deleteTodo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range todos {
+		if item.ID == params["id"] {
+			fmt.Println("here")
+			todos = append(todos[:index], todos[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(todos)
+}
+
+func RootEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Write([]byte("anything e.g hello :)"))
+}
+
+func main() {
+	//Init Router
+	router := mux.NewRouter()
+
+	//Route Handlers / Endpoints
+	router.HandleFunc("/api/todo", getTodos).Methods("GET")
+	router.HandleFunc("/api/todo/{id}", getTodo).Methods("GET")
+	router.HandleFunc("/api/todo", createTodo).Methods("POST")
+	router.HandleFunc("/api/todo/{id}", updateTodo).Methods("PUT")
+	router.HandleFunc("/api/todo/{id}", deleteTodo).Methods("DELETE")
+
+	//start server
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
