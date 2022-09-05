5a595b7bfcd68b43dbde7af6583ebdb23cd9e968
ghazal Pouresfandiyar <ghazal.brn@gmail.com>
2022-09-05 12:02:02 +0430

    add script having add option

---
+++ Script/todolist.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+case $1 in
+	add)
+		case $2 in
+			-t | --title)
+				if [ -z $3 ]; then
+					echo "Option -t|--title Needs a Parameter"
+				fi
+				;;
+			*)
+				;;
+		esac
+		case $4 in
+			-p | --priority)
+				if [ -z $5 ]; then
+					priority="L"
+				elif [ $5 == 'M' ] || [ $5 == 'L' ] || [ $5 == 'H']; then
+					priority=$5
+				else
+					echo "Option -p|--priority Only Accept L|M|H"
+				fi
+				;;
+			*)
+				priority="L"
+				;;
+		esac
+		echo "0,$priority,$3" >>  tasks.csv
+	;;
+esac
